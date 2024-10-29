09240ede56c1e8074200b3b189ba3b74afe1c25a
realfabecker <realfabecker@outlook.com>
2024-10-29 19:43:53 -0300

    0

---
+++ classes/c0301-fbackn/domain/src/domain/forum/application/use-cases/answer-question.ts
@@ -0,0 +1,29 @@
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+export class AnswerQuestionUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+    })
+
+    await this.answersRepository.create(answer)
+
+    return answer
+  }
+}
